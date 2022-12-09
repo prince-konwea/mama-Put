2c03b68a8cdb617dcb09904f115d646bb88388b9
prince-konwea <kokodecontee123@gmail.com>
2022-12-10 00:35:16 +0100

    created home, pages and other sub components for project

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Veggie from "../Veggie";
+import Popular from "../Popular";
+
+
+
+function Home() {
+  return (
+    <div>
+    <Veggie />
+    <Popular/>    
+    </div>
+  )
+}
+
+export default Home;
