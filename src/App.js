57e66df7937f0656f628825ef619ee609af36f37
prince-konwea <kokodecontee123@gmail.com>
2022-12-10 08:53:39 +0100

    add Navlink to category component and styled with styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Pages from "./components/pages/Pages";
 import Category from "./components/Category";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Category />
       <Pages />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,27 +1,28 @@
 import {FaPizzaSlice, FaHamburger} from "react-icons/fa";
-import { GiNoodles, GiChopsticks }  from "react-icons/gi";
+import { GiNoodles, GiChopsticks, GiWhiteBook }  from "react-icons/gi";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
+import { scryRenderedComponentsWithType } from "react-dom/test-utils";
 
 function Category() {
   return (
     <List>
-      <div>
+      <SLink to={"/cuisine/Italian"}>
         <FaPizzaSlice />
         <h4>Italian</h4>
-      </div>
-      <div>
+      </SLink>
+      <SLink to={"/cuisine/American"}>
         <FaHamburger />
         <h4>American</h4>
-      </div>
-      <div>
+      </SLink>
+      <SLink to={"/cuisine/Thai"}>
         <GiNoodles />
         <h4>Thai</h4>
-      </div>
-      <div>
+      </SLink>
+      <SLink to={"/cuisine/Japanese"}>
         <GiChopsticks />
         <h4>Japanese</h4>
-      </div>
+      </SLink>
     </List>
   )
 }
@@ -31,5 +32,39 @@ display: flex;
 justify-content:center;
 margin: 2rem 0rem;
 `
+const SLink = styled(NavLink)`
+display: flex;
+flex-direction:column;
+justify-content: center;
+align-items: center;
+border-radius: 50%;
+margin-right: 2rem;
+text-decoration: none;
+background: linear-gradient(35deg, #494949, #313131);
+width: 6rem;
+height: 6rem;
+cursor: pointer;
+transform: scale(0.8);
+
+h4{
+  color: white;
+  font-size: 0.8rem;
+}
+svg{
+  color: white;
+  font-size: 1.5rem;
+}
+&.active{
+  background: linear-gradient(to right, #f27121, #e94057);
+  svg{
+    color: white;
+  }
+  h4{
+    color:white;
+  }
+}
+`
+
+
 
 export default Category;
